41ed41485a7f241ab1cd0fe89618810403c9b54d
Mikhail Murunov <haskiindahouse@gmail.com>
2021-02-19 23:24:47 +0300

    nothing changed

--- mystack.js
+++ mystack.js
@@ -30,30 +30,3 @@ class MyStack {
     return this.elem[this.length - 1]
   }
 }
-let stack = new MyStack()
-
-let checker = new MyStack()
-let oppBrackets = ['(', '[', '{']
-let clsBrackets = [')', ']', '}']
-for(let i = 0; i < stack.length; i++){
-  console.log(stack.elem[i])
-  if (oppBrackets.includes(stack.elem[i])){
-    checker.Push(stack.elem[i])
-  }
-  if (clsBrackets.includes(stack.elem[i])){
-    if (checker.length){
-      if (oppBrackets.indexOf(checker.Top()) === clsBrackets.indexOf(stack.elem[i])){
-        checker.Pop()
-      }
-    }
-    else{
-        console.log("Wrong brackets!")
-    }
-  }
-}
-if (checker.length == 0){
-   console.log("Right brackets!")
-}
-else{
-  console.log("Wrong brackets!")
-}
