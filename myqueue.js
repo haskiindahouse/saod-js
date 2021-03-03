6dd49b207962a6d3271398ef2082e176d6f750ee
Mikhail Murunov <haskiindahouse@gmail.com>
2021-03-03 18:54:43 +0300

    nothings change

--- myqueue.js
+++ myqueue.js
@@ -1,7 +1,7 @@
 class MyQueue {
   constructor() {
     this.elem = []
-    this.length = 0;
+    this.length = 0
     this.first = NaN
   }
   Clear() {
@@ -16,11 +16,11 @@ class MyQueue {
     this.length -= 1
     let pop_elem = this.first
     this.elem = this.elem.slice(1)
-    this.first = this.elem[0]
+    this.first = this.elem
     return pop_elem
   }
-  Push(element, position) {
-    this.elem[this.length] = [element, position]
+  Push(element) {
+    this.elem[this.length] = element
     if (!this.length)
     {
       this.first = this.elem[this.length]
@@ -28,20 +28,6 @@ class MyQueue {
     this.length += 1
   }
   Top() {
-    return this.first[1]
+    return this.first
   }
 }
-
-setTimeout(sayHi, 1000);
-let test = new MyQueue()
-test.Push(2)
-test.Push(4)
-test.Push(15)
-console.log(test.isEmpty())
-console.log(test.Top())
-console.log(test.Pop())
-console.log(test)
-test.Pop()
-console.log(test)
-test.Push(144)
-console.log(test.Top())
